e09e0d71f3ac161adc903cf21872ca4b5e1937db
Lewis <lewis_whitear@outlook.com>
2021-11-01 23:27:31 +0000

    Initial commit

---
+++ experimental/portappb/src/main/java/com/com619/group6/PortappbApplication.java
@@ -0,0 +1,13 @@
+package com.com619.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortappbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortappbApplication.class, args);
+	}
+
+}
