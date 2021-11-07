6de248115d6b524c69089c48f77a5a27ffdbf3e7
Lewis <lewis_whitear@outlook.com>
2021-11-07 22:41:37 +0000

    Scheduling and tidal functions

---
+++ experimental/portappb/src/main/java/com/com619/group6/model/Berth.java
@@ -0,0 +1,27 @@
+package com.com619.group6.model;
+
+/**
+ * Model class representing a berth at the port.
+ * 
+ * @author WhitearL
+ */
+public class Berth {
+
+	private int berthID;
+
+	
+	/**
+	 * @return the berthID
+	 */
+	public int getBerthID() {
+		return berthID;
+	}
+
+	/**
+	 * @param berthID the berthID to set
+	 */
+	public void setBerthID(int berthID) {
+		this.berthID = berthID;
+	}
+	
+}
