c26b26cf25b531df712e280d24eaa00ab38da2e6
Grahame White <4whitg16@solent.ac.uk>
2021-12-02 12:15:32 +0000

    Fixed lint issues in changed code

--- experimental/portappb/src/test/java/com/com619/group6/jpadao/DaoFactoryJpaTests.java
+++ experimental/portappb/src/test/java/com/com619/group6/jpadao/DaoFactoryJpaTests.java
@@ -2,6 +2,7 @@ package com.com619.group6.jpadao;
 
 import com.com619.group6.model.dao.BerthDao;
 import com.com619.group6.model.dao.DaoFactory;
+import com.com619.group6.model.dao.PortServiceOrderDao;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,5 +20,19 @@ public class DaoFactoryJpaTests {
         BerthDao berthDao = factory.getBerthDao();
 
         assertThat(berthDao, instanceOf(BerthDaoJpa.class));
+    }
+
+    /**
+     * Check that a PortServiceOrderJpa instance is created when
+     * getPortServiceOrderDao() is called.
+     */
+    @Test
+    public void getPortServiceOrderDaoReturnsPortServiceOrderDaoJpaInstance()
+    {
+        DaoFactory factory = new DaoFactoryJpa();
+
+        PortServiceOrderDao psoDao = factory.getPortServiceOrderDao();
+
+        assertThat(psoDao, instanceOf(PortServiceOrderDaoJpa.class));
     }
 }
